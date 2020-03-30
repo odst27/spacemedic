b85a525c8d93b4e3e770f9342c992089d25d5639
Dev Kavathekar <dkavathe@dkavathe.remote.csb>
2020-03-30 16:14:38 -0400

    new scripts added

--- medic_app_cli/main.py
+++ medic_app_cli/main.py
@@ -1,6 +1,28 @@
-from lxml import html
-from cssselect import GenericTranslator, SelectorError
+import urllib2
+from bs4 import BeautifulSoup
 
-file = open("https://www.miemssalert.com/chats/Default.aspx?hdRegion=5&hdtab=Hospitals", "r")
-doc = html.document_fromstring(file.read())
-print(doc.cssselect('title')[0].text_content())
+html = urllib2.urlopen("https://www.miemssalert.com/chats/Default.aspx?hdRegion=5&hdtab=Hospitals").read()
+soup = BeautifulSoup(html,features="lxml")
+
+tds = soup.findAll("td", {"class": "Chats"})
+pageblk = soup.get_text()
+startpos = pageblk.find("HospitalYellow")
+endpos = pageblk.find("CountyBlue")
+
+
+#print pageblk
+
+
+hospitalblk = pageblk[startpos+63:endpos]
+reg5 = []
+
+for hospital in hospitalblk.strip("\n").splitlines():
+   reg5.append(hospital)
+   if ":" in hospital:
+     print "**************************"
+     print hospital[hospital.find(":")-2:]
+
+for val in reg5:
+   print val 
+
+
