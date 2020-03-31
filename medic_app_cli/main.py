fba6dc546db07b560bc142414ad4553878e05e82
Dev Kavathekar <dkavathe@dkavathe.remote.csb>
2020-03-31 17:20:35 -0400

    Integrating ArcGIS, testing without flask app, running into pip install arcgis module errors

--- medic_app_cli/main.py
+++ medic_app_cli/main.py
@@ -1,35 +1,43 @@
 import urllib2
+import logging
+import inspect
 from bs4 import BeautifulSoup
-from flask import Flask
+#from flask import Flask
+from arcgis.gis import GIS
 
-app = Flask(__name__)
+#app = Flask(__name__)
 
-@app.route("/")
-def hello():
+#@app.route("/")
+#def hello():
    
-   html = urllib2.urlopen("https://www.miemssalert.com/chats/Default.aspx?hdRegion=5&hdtab=Hospitals").read() 
+   #html = urllib2.urlopen("https://www.miemssalert.com/chats/Default.aspx?hdRegion=5&hdtab=Hospitals").read() 
    #html = urllib2.urlopen("https://www.miemssalert.com/chats/Default.aspx?hdRegion=3&hdtab=Hospitals").read()
-   #html = urllib2.urlopen("https://www.miemssalert.com/chats/Default.aspx?hdRegion=124&hdtab=Hospitals").read()
+html = urllib2.urlopen("https://www.miemssalert.com/chats/Default.aspx?hdRegion=124&hdtab=Hospitals").read()
 
-   soup = BeautifulSoup(html,features="lxml")
+soup = BeautifulSoup(html,features="lxml")
    
-   table = soup.find("table", {"id": "tblHospitals"})
-   table_data = table.find_all("tr")
-   table_data.pop(0)
+table = soup.find("table", {"id": "tblHospitals"})
+table_data = table.find_all("tr")
+table_data.pop(0)
    
-   hospitals = {}
-   i = 0
+hospitals = {}
+i = 0
    
-   for unit in table_data:
-      alerts = unit.find_all('td')
-      hospitals[i] = []
-      j = 0
-      for flag in alerts:
-         if j < 7:
-            hospitals[i].append(flag.string)
-            j = j + 1
-         
+for unit in table_data:
+   alerts = unit.find_all('td')
+   hospitals[i] = []
+   j = 0
+   for flag in alerts:
+      if j < 7:
+         hospitals[i].append(flag.string)
+         j = j + 1
       
-      i = i + 1
    
-   return hospitals
+   i = i + 1
+
+
+gis = GIS()
+webmap = gis.content.get('41281c51f9de45edaf1c8ed44bb10e30')
+webmap
+  
+#return hospitals
