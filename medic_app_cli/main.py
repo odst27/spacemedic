ee2efc9ebf6f68c9075275086895997127eecbf7
Dev Kavathekar <dkavathe@dkavathe.remote.csb>
2020-03-31 10:10:38 -0400

    completed algo...6 elements, 8 given elements not changing first and last for consistency

--- medic_app_cli/main.py
+++ medic_app_cli/main.py
@@ -10,11 +10,25 @@ table_data = table.find_all("tr")
 table_data.pop(0)
 # works 26 elements print len(table_data)
 
-hospitals = []
-for td in table_data:
-   print "**********"
-   for tags in td:
-      print tags
-   print "**********"
+hospitals = {}
+key = 0
+dataarr = []
+
+# 8 cells per row, 6 valid elements. keeping invalid element pos: 0 and 7 as 'u\n'
+for tr in table_data:   
+   for td in tr:
+      td = td.strip('\n')
+      for txt in td:
+         dataarr.append(txt)
+      print dataarr     
+      del dataarr[:]
+      #hospitals[key] = dataarr
+      #dararr = []
+   #key = key + 1
 
 
+print hospitals
+#print len(hospitals)
+
+#for unit in hospitals:
+#   print unit 
