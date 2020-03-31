478de3ac7146fcfaad94132ae193630bd192dc6d
Dev Kavathekar <dkavathe@dkavathe.remote.csb>
2020-03-31 15:49:09 -0400

    began Flask app framework impl

--- medic_app_cli/main.py
+++ medic_app_cli/main.py
@@ -1,25 +1,32 @@
 import urllib2
 from bs4 import BeautifulSoup
+from flask import Flask
 
-html = urllib2.urlopen("https://www.miemssalert.com/chats/Default.aspx?hdRegion=5&hdtab=Hospitals").read()
-soup = BeautifulSoup(html,features="lxml")
+app = Flask(__name__)
 
-table = soup.find("table", {"id": "tblHospitals"})
-table_data = table.find_all("tr")
-table_data.pop(0)
-
-hospitals = {}
-i = 0
-
-for unit in table_data:
-   alerts = unit.find_all('td')
-   hospitals[i] = []
-   j = 0
-   for flag in alerts:
-      if j < 7:
-         hospitals[i].append(flag.string)
-         j = j + 1
+@app.route("/")
+def hello():
+   
+   html = urllib2.urlopen("https://www.miemssalert.com/chats/Default.aspx?hdRegion=5&hdtab=Hospitals").read()
+   soup = BeautifulSoup(html,features="lxml")
+   
+   table = soup.find("table", {"id": "tblHospitals"})
+   table_data = table.find_all("tr")
+   table_data.pop(0)
+   
+   hospitals = {}
+   i = 0
+   
+   for unit in table_data:
+      alerts = unit.find_all('td')
+      hospitals[i] = []
+      j = 0
+      for flag in alerts:
+         if j < 7:
+            hospitals[i].append(flag.string)
+           j = j + 1
+         
       
-   i = i + 1
-
-print hospitals
+      i = i + 1
+   
+   return hospitals
