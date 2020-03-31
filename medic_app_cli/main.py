5166c41f9cd69bc1fc47af319163ca84d289208a
Dev Kavathekar <dkavathe@dkavathe.remote.csb>
2020-03-31 08:52:21 -0400

    updated parser with proper more efficent hospital data capture, noticed 5 alerts were concatenated 23:02:05:12, altered this algo, storing these table tags into data structure...hashmap?..tbd WIP

--- medic_app_cli/main.py
+++ medic_app_cli/main.py
@@ -4,15 +4,30 @@ from bs4 import BeautifulSoup
 html = urllib2.urlopen("https://www.miemssalert.com/chats/Default.aspx?hdRegion=5&hdtab=Hospitals").read()
 soup = BeautifulSoup(html,features="lxml")
 
-tds = soup.findAll("td", {"class": "Chats"})
-pageblk = soup.get_text()
-startpos = pageblk.find("HospitalYellow")
-endpos = pageblk.find("CountyBlue")
+table = soup.find("table", {"id": "tblHospitals"})
+table_data = table.find_all("tr")
+
+# works 26 elements print len(table_data)
+
+names = []
+for td in table_data:
+   print "**********"
+   print td
+   print "**********"
+   #names.append(td.b.text.replace('\n,' ' ').strip())
+
+#print names
+
+#print table
+
+#pageblk = soup.get_text()
+#startpos = pageblk.find("HospitalYellow")
+#endpos = pageblk.find("CountyBlue")
 
 
 #print pageblk
 
-
+'''
 hospitalblk = pageblk[startpos+63:endpos]
 reg5 = []
 
@@ -20,9 +35,10 @@ for hospital in hospitalblk.strip("\n").splitlines():
    reg5.append(hospital)
    if ":" in hospital:
      print "**************************"
-     print hospital[hospital.find(":")-2:]
+     print hospital + "status: " + hospital[hospital.find(":")-2:]
 
 for val in reg5:
    print val 
 
+'''
 
