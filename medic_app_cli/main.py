5f815d4d3d7fc1ae33d0884bd35101107bc4f42a
Dev Kavathekar <dkavathe@dkavathe.remote.csb>
2020-03-31 09:00:33 -0400

    updated algo now stores modular table into matrix format

--- medic_app_cli/main.py
+++ medic_app_cli/main.py
@@ -7,38 +7,14 @@ soup = BeautifulSoup(html,features="lxml")
 table = soup.find("table", {"id": "tblHospitals"})
 table_data = table.find_all("tr")
 
+table_data.pop(0)
 # works 26 elements print len(table_data)
 
-names = []
+hospitals = []
 for td in table_data:
    print "**********"
-   print td
+   for tags in td:
+      print tags
    print "**********"
-   #names.append(td.b.text.replace('\n,' ' ').strip())
 
-#print names
-
-#print table
-
-#pageblk = soup.get_text()
-#startpos = pageblk.find("HospitalYellow")
-#endpos = pageblk.find("CountyBlue")
-
-
-#print pageblk
-
-'''
-hospitalblk = pageblk[startpos+63:endpos]
-reg5 = []
-
-for hospital in hospitalblk.strip("\n").splitlines():
-   reg5.append(hospital)
-   if ":" in hospital:
-     print "**************************"
-     print hospital + "status: " + hospital[hospital.find(":")-2:]
-
-for val in reg5:
-   print val 
-
-'''
 
